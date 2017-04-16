f8cc0f6265ed310683ff254bbd4836f2047bf938
jab <Jacob Bennett>
2017-04-16 08:39:23 -0500

    Add element before script execution

--- d3notebook/d3notebook.py
+++ d3notebook/d3notebook.py
@@ -41,7 +41,7 @@ class D3Notebook(object):
         data = data[0]
         js = self._bind_js(js, "\"" + "g" + "\"", data)
 
-        html_string = "<style>{}</style>\n <script>{}</script>\n {}".format(css, js, html)
+        html_string = "{}\n <style>{}</style>\n <script>{}</script>".format(html, css, js)
         display(HTML(html_string))
 
     def _bind_js(self, js, selector, data):
