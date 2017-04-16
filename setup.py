50a9540de04004bef1c4bb70a193716a56bbd66c
jab <Jacob Bennett>
2017-04-16 08:06:10 -0500

    Upgrade version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_install_requirements(path):
 setup(
     name="d3notebook",
     packages=find_packages(),
-    version="0.0.5",
+    version="0.0.6",
     author="Jacob Bennett",
     description="test",
     url='http://github.com',
